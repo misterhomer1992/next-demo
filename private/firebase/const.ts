998cbd7ca33aa66b2e369cc1f104ed9b04430d8f
mariuschik <misterhomer1992@gmail.com>
2022-06-02 13:08:44 +0300

    Remove private from gitignore.

---
+++ private/firebase/const.ts
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyCbbSMvKlazDA3lvbcMmW-FOdEuHiFmd5U",
+    authDomain: "next-demo-2c4ea.firebaseapp.com",
+    databaseURL: "https://next-demo-2c4ea-default-rtdb.firebaseio.com",
+    projectId: "next-demo-2c4ea",
+    storageBucket: "next-demo-2c4ea.appspot.com",
+    messagingSenderId: "133253165337",
+    appId: "1:133253165337:web:4f332bd487db939a1b1588"
+};
